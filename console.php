9f5591a0f72d19f671fd3f09b5d616b48f6ecf43
mkas <mvkasatkin@gmail.com>
2017-11-10 02:24:13 +0700

    dev

--- console.php
+++ console.php
@@ -2,21 +2,19 @@
 
 use Symfony\Component\Console\Application;
 
-(function () {
-    require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
-    defined('ENV') or define('ENV', 'dev');
+defined('ENV') or define('ENV', 'dev');
 
-    /** @var array[] $config */
-    $config = require __DIR__ . '/config/config.php';
-    $application = new \WebComplete\mvc\Application($config, false);
-    $container = $application->getContainer();
+/** @var array[] $config */
+$config = require __DIR__ . '/config/config.php';
+$application = new \WebComplete\mvc\Application($config, false);
+$container = $application->getContainer();
 
-    $console = new Application();
-    if (isset($config['commands'])) {
-        foreach ($config['commands'] as $commandClass) {
-            $console->add($container->get($commandClass));
-        }
+$console = new Application();
+if (isset($config['commands'])) {
+    foreach ($config['commands'] as $commandClass) {
+        $console->add($container->get($commandClass));
     }
-    $console->run();
-})();
+}
+$console->run();
--- web/index.php
+++ web/index.php
@@ -1,3 +1,14 @@
 <?php
 
-echo 'WebApplication index here';
+use WebComplete\mvc\Application;
+use WebComplete\mvc\front\FrontController;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+defined('ENV')
+or define('ENV', in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'], false) ? 'dev' : 'prod');
+
+$config = require __DIR__ . '/../config/config.php';
+$application = new Application($config);
+$front = $application->getContainer()->get(FrontController::class);
+$front->dispatch()->send();
