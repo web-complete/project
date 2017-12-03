b271ef5ccf81c9b2809813ba48fa7ecd56eb249b
mkas <mvkasatkin@gmail.com>
2017-12-03 23:30:07 +0700

    dev

--- admin/Cube.php
+++ admin/Cube.php
@@ -2,10 +2,7 @@
 
 namespace admin;
 
-use admin\assets\AdminAsset;
 use WebComplete\core\cube\AbstractCube;
-use WebComplete\core\utils\container\ContainerInterface;
-use WebComplete\mvc\assets\AssetManager;
 
 class Cube extends AbstractCube
 {
