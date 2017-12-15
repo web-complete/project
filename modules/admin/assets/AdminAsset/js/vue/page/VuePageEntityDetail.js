914a1bd0375a4b71d87232374d7047affae264a7
Maxim Kasatkin <mvkasatkin@gmail.com>
2017-12-15 19:41:48 +0700

    dev

--- modules/admin/assets/AdminAsset/js/vue/page/VuePageEntityDetail.js
+++ modules/admin/assets/AdminAsset/js/vue/page/VuePageEntityDetail.js
@@ -1,3 +1,37 @@
 VuePageEntityDetail = {
-    template: ``
+    template: `
+<div class="page block">
+    <div class="page-detail">
+        <div class="page-top" v-show="isLoaded">
+            <h1>
+                <router-link class="back" :to="'/list/'+entityName"><i class="ion-chevron-left"></i></router-link>
+                {{title}}
+                <small v-if="entityId">#{{entityId}}</small>
+            </h1>
+        </div>
+                
+        <transition name="fade">
+            <vue-entity-form>
+            
+            </vue-entity-form>
+        </transition>
+    </div>
+</div>
+    `,
+    data(){
+        return {
+            title: ''
+        }
+    },
+    computed: {
+        isLoaded(){
+            return this.title;
+        },
+        entityName(){
+            return this.$route.params.entity;
+        },
+        entityId(){
+            return this.$route.params.id;
+        }
+    }
 };
